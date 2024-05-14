d53ed379170ac3d9953a07418c626b224511522d
Khezac <khezac1@gmail.com>
2024-05-14 12:05:51 -0300

    commit inicial

---
+++ biblioteca-incrementada/src/main/java/br/org/serratec/projetobiblioteca/bibliotecaincrementada/BibliotecaIncrementadaApplication.java
@@ -0,0 +1,13 @@
+package br.org.serratec.projetobiblioteca.bibliotecaincrementada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaIncrementadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaIncrementadaApplication.class, args);
+	}
+
+}
---
+++ biblioteca-incrementada/src/test/java/br/org/serratec/projetobiblioteca/bibliotecaincrementada/BibliotecaIncrementadaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.projetobiblioteca.bibliotecaincrementada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaIncrementadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
